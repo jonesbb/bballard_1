28fe154bd5948d3ebfb85b9051fc8f2af4f279a9
jonesbb <jonesbb@gmail.com>
2012-02-18 17:56:08 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bballard1::Application.config.secret_token = '1fb0079410b90a465bb1c996d1374c549ec37145e4d5a755d4929c6bd8e93a65c8dc0669e8970b28c08e20559737c89e43fd077f368256c65ab6b54d8ecb41c1'
---
+++ public/js/jb_olio.js
@@ -0,0 +1,69 @@
+var jb_olio = [
+		{
+		featured: 'rails_demo/rails_demo_thumb.jpg',
+		title: 'Ruby On Rails Microblog',
+		description: '<strong>ROLE: </strong>Interaction Designer, Conceptor</br> <strong>ASK: </strong>PUMA wanted to make their customization business to be social in nature. We took social shopping and PUMA-fied it. These documents were designed to communicate the concept at a casual level.  ',
+		filters: ['web','app'],
+		more: [
+			'rails_demo/Slide3.jpg',
+			'rails_demo/Slide5.jpg'
+	
+		]
+
+	},
+
+	{
+		featured: 'frpf/frpf_thumb.jpg',
+		title: 'Front Range Powder Factory',
+		description: '<strong>ROLE:</strong> Interaction Design, Concepting </BR><strong>THE ASK:</strong> How to create a quick fix web presence to the PUMA IPad app, only found in retail stores. We used the data collected from the app to vizualize info from each store. Featured stores slide into focus based on where the hero shoe was designed.',
+		filters: ['service', 'vida'],
+		more: [
+			'frpf/calendar.2012.small.jpg',
+			'frpf/hut.group.2005.jpg',
+			'frpf/frpf_story.jpg'
+		]
+
+	},
+
+
+
+	{
+		featured: 'jpl_nustar/nus_thumb.jpg',
+		title: 'NuSTAR',
+		description: '<strong>ROLE:</strong> Interaction Design, Architecture </BR><strong>THE ASK:</strong> PUMAs customization experience required its own Ecommerce and fulfillment organization. We architected it, not without a plethera of documentation.',
+		filters: ['flight'],
+		more: [
+			'jpl_nustar/nus_1.jpg',
+			'jpl_nustar/nus_2.jpg',
+			'jpl_nustar/nus_3.jpg'
+		]
+
+	},
+	{
+		featured: 'ressignol/thumb.jpg',
+		title: 'Ressignol',
+		description: '<strong>ROLE:</strong> Visual Design, Concepting </BR><strong>THE ASK:</strong> Rebrand a company. I chose Rossignol, an older-school ski company that needed some new-school flavor. I embedded an experience in the site where Users can upload Xray photos of their gnarly falls.',
+		filters: ['flight', 'web', 'school'],
+		more: [
+			'ressignol/rossy.jpg',
+			'ressignol/love.jpg',
+			'ressignol/ecomm.jpg'
+		]
+
+	},
+	{
+		featured: 'jpl_phx/phx_thumb1.jpg',
+		title: 'Phoenix',
+		description: '<strong>ROLE:</strong> Visual Design, Concepting </BR><strong>THE ASK:</strong> Rebrand a company. I chose Rossignol, an older-school ski company that needed some new-school flavor. I embedded an experience in the site where Users can upload Xray photos of their gnarly falls.',
+		filters: ['flight'],
+		more: [
+			'jpl_phx/phx_1.jpg',
+			'jpl_phx/phx_2.jpg',
+			'jpl_phx/phx_3.jpg',
+			'jpl_phx/phx_4.jpg'
+		]
+
+	},
+
+
+];
